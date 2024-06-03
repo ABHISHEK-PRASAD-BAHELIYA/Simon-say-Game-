7b6655cb17a05ebe4537427b8941439147d7d16b
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-06-03 11:42:30 +0530

    Flash button & Level Up

--- oldCode/app.js
+++ oldCode/app.js
@@ -1,12 +1,39 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btn = ["yellow", "purple", "green", "red"];
+
 let started = false;
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", () =>{
     if(started == false) {
         console.log("game started!");
         started = true;
+
+        levelUp();
     }
 });
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 200);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    //random btn flash
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btn[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
